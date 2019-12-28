8f038d351c375a67e38563226befc85cf4c08fe5
Zach Lang <zach.d.lang@gmail.com>
2019-12-29 11:59:04 +1300

    Switching to pip package

---
+++ README.md
@@ -0,0 +1,3 @@
+# Flasktools
+
+A series of basic functions for handling personal Flask websites.
---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name='flasktools',
+	version='1.0.0',
+	author='Zachary Lang',
+	author_email='zach.d.lang@gmail.com',
+	description='Utilities for Flask websites',
+	long_description=long_description,
+	long_description_content_type='text/markdown',
+	url='https://github.com/zachdlang/flasktools',
+	packages=setuptools.find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+	install_requires=[
+		'flask',
+		'Pillow',
+		'passlib',
+		'celery',
+		'psycopg2'
+	]
+)
