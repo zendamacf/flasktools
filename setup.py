6d5d96c31b7e11d72ad35f57b015bfdd3398b7e9
Zach Lang <zach.d.lang@gmail.com>
2020-04-29 00:37:06 +1200

    Updating GitHub URLs

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
 	description='Utilities for Flask websites',
 	long_description=long_description,
 	long_description_content_type='text/markdown',
-	url='https://github.com/zachdlang/flasktools',
+	url='https://github.com/zendamacf/flasktools',
 	packages=setuptools.find_packages(),
 	classifiers=[
 		"Programming Language :: Python :: 3",
