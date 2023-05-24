1601a2437c0c26269a04e9f2c34578a5d0d7b28a
Zach Lang <zach.d.lang@gmail.com>
2023-05-25 00:24:02 +1200

    Release 2.0.1
    Fixes missing Redis dependency.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
 	name='flasktools',
-	version='2.0.0',
+	version='2.0.1',
 	author='Zachary Lang',
 	author_email='zach.d.lang@gmail.com',
 	description='Utilities for Flask websites',
@@ -29,5 +29,6 @@ setuptools.setup(
 		'celery>=5.2.7',
 		'psycopg2>=2.9.6',
 		'pyjwt>=2.7.0'
+		'redis>=4.5.5'
 	]
 )
